badd950432e6c92b52e4fd08b677b030d78dc174
LamNV - Dev <nguyenvietlam290304@gmail.com>
2026-05-08 21:18:20 +0700

    [feat] add configuration

---
+++ E-CommerceRookie.BE/BackendAPI/MidAssignment.Infrastructure/Persistences/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using MidAssignment.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MidAssignment.Infrastructure.Persistences.Configurations
+{
+    public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(c => c.Email)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            builder.HasIndex(c => c.Email).IsUnique();
+
+            builder.Property(c => c.Phone)
+                .HasMaxLength(20);
+        }
+    }
+}
