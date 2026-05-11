4b6a0555e9c165886211e5de16467228294ed7ca
LamNV - Dev <nguyenvietlam290304@gmail.com>
2026-05-11 13:55:33 +0700

    [feat] customer site UI

---
+++ E-CommerceRookie.BE/BackendAPI/MidAssignment.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MidAssignment.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
