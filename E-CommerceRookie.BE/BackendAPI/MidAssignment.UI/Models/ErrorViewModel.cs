75df2721ad40d7995463b50b387a6b918d439943
LamNV - Dev <nguyenvietlam290304@gmail.com>
2026-05-12 09:44:01 +0700

    [feat] image asset

--- E-CommerceRookie.BE/BackendAPI/MidAssignment.UI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MidAssignment.UI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- E-CommerceRookie.FE/src/features/product/productSlice.ts
+++ E-CommerceRookie.FE/src/features/product/productSlice.ts
@@ -1,4 +1,4 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+    import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import type { PayloadAction } from '@reduxjs/toolkit'
 import { productService } from './services'
 import type { ProductState, PagedResult, Product } from './types'
